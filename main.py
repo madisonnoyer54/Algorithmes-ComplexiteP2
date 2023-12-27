95605001b60eb6869c56857870608faeb1fc2cd9
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2023-12-27 14:54:03 +0100

    lecteur fichier OK

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def remplirPoids(nb):
     objects = []
     poid = 10
@@ -11,9 +14,10 @@ def remplirPoids(nb):
             objects.append((3,poid))
         poid = poid +1
 
-    poid = 10
+
     # Paquet qui reste 
     for k in range(reste):
+        poid = random.randint(10, 50)
         objects.append((3,poid))
         poid = poid +1
    
@@ -31,7 +35,11 @@ def FractionalPacking(listObjets, tailleBoite):
 
     return quotient + (1 if reste != 0 else 0) # Rajoute 1 boite si il y a des reste 
 
-def FirstFitDecreasingPacking():
+
+# Q2
+def FirstFitDecreasingPacking(listObjets, tailleBoite):
+    listeTuples =[]
+
     return 3
 
 
@@ -40,6 +48,8 @@ def BestFitDecreasingPacking():
 
 
 # On considère la capacité de chaque bin H = 150
+
+## QUESTION 1
 # Exemple d'utilisation pour 125 
 objects = remplirPoids(125)
 result = FractionalPacking(objects, 150)
@@ -64,5 +74,31 @@ print("Nombre de boîtes utilisées:", result)
 
 
 
+## QUESTION 2 
+def lire_fichier(nom_fichier):
+    with open(nom_fichier, 'r') as fichier:
+        lignes = fichier.readlines()
+        list = []
+
+        for ligne in lignes:
+            # Vérifier que la ligne commence par 'e'
+            if ligne.startswith('e'):
+                try:
+                    
+                    decomposeligne = ligne.split(" ")
+                    list.append((decomposeligne[1],decomposeligne[2]))
+                except ValueError:
+                    print(f"Erreur")
+
+    return list
+# Lire les objets à partir du fichier
+list = lire_fichier("DSJC500.5.txt")
+
+
+#print(list)
+
+
+# Trie les objets par poid décroissante
+sorted_objects = sorted(objects, key=lambda x: x[1], reverse=True)
 
 
